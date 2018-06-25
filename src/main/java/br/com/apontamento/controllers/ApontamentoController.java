f51dc9dee06342229960001c9b02e12e9dbe2c8b
raulzitoo <raulzito.j@gmail.com>
2018-06-25 20:35:06 -0300

    Ajustando o Mapping do controller

--- src/main/java/br/com/apontamento/controllers/ApontamentoController.java
+++ src/main/java/br/com/apontamento/controllers/ApontamentoController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/apontamento/")
+@RequestMapping("/")
 public class ApontamentoController {
 
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView listaApontamento() {
 		
-		System.out.println("Entrei no lista");
+		
 		return new ModelAndView("listaApontamento");
 	}
 	
